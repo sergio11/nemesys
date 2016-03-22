69932130096bcfea65fe87811a9a83a65eb2e48b
Sergio11 <sss4esob@gmail.com>
2016-03-22 20:18:37 +0100

    using eact-router-transition

--- app/components/App/App.rt.js
+++ app/components/App/App.rt.js
@@ -1,6 +1,26 @@
 import React from 'react/addons';
 import _ from 'lodash';
 import Navbar from '../Navbar/Navbar';
+import { RouteTransition } from 'react-router-transition';
 export default function () {
-    return React.createElement('div', {}, React.createElement(Navbar, {}), React.createElement('main', { 'className': 'main' }, '\r\n        ', this.props.children, '\r\n    '));
+    return React.createElement('div', {}, React.createElement(Navbar, {}), React.createElement(RouteTransition, {
+        'component': 'main',
+        'pathname': this.props.location.pathname,
+        'atEnter': {
+            translateX: -100,
+            opacity: 0.7
+        },
+        'atLeave': {
+            translateX: 100,
+            opacity: 0
+        },
+        'atActive': {
+            translateX: 0,
+            opacity: 1
+        },
+        'mapStyles': styles => ({
+            opacity: styles.opacity,
+            transform: `translateX(${ styles.translateX }%)`
+        })
+    }, '\r\n        ', this.props.children, '\r\n    '));
 };
