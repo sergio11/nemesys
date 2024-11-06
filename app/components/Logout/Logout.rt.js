709bae28c35a41b83af6e8c3c3cd0b1ff6769d93
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-11-06 19:42:10 +0100

    Initial commit

--- app/components/Logout/Logout.rt.js
+++
@@ -1,9 +0,0 @@
-import React from 'react/addons';
-import _ from 'lodash';
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import Alert from 'react-bootstrap/lib/Alert';
-export default function () {
-    return React.createElement('div', {}, this.props.user ? React.createElement(Alert, { 'bsStyle': 'info' }, React.createElement('div', { 'className': 'alert-icon' }, React.createElement('i', { 'className': 'material-icons' }, 'info_outline')), React.createElement('span', {}, 'Cerrando sesión, espere un momento')) : null, !this.props.user ? React.createElement(Alert, { 'bsStyle': 'success' }, React.createElement('div', { 'className': 'alert-icon' }, React.createElement('i', { 'className': 'material-icons' }, 'info_outline')), React.createElement('span', {}, 'Sesión Cerrada con éxito')) : null);
-};
--- app/firebase.js
+++
@@ -1,4 +0,0 @@
-import Rebase from 're-base';
-var base = Rebase.createClass('https://instantyfire.firebaseio.com/');
-export default base;
-
